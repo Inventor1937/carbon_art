RadonX-MM/art

tests: All or nothing
Signed-off-by: Joe Maples <frap129@gmail.com>

build/Android.common_test.mk
@@ -35,76 +35,76 @@ ART_TEST_KNOWN_BROKEN :=
 ART_TEST_KNOWN_FAILING :=
 
 # Keep going after encountering a test failure?
-ART_TEST_KEEP_GOING ?= true
+ART_TEST_KEEP_GOING := true
 
 # Do you want all tests, even those that are time consuming?
 ART_TEST_FULL ?= false
 
 # Do you want default compiler tests run?
-ART_TEST_DEFAULT_COMPILER ?= true
+ART_TEST_DEFAULT_COMPILER := $(ART_TEST_FULL)
 
 # Do you want interpreter tests run?
-ART_TEST_INTERPRETER ?= $(ART_TEST_FULL)
+ART_TEST_INTERPRETER := $(ART_TEST_FULL)
 
 # Do you want JIT tests run?
-ART_TEST_JIT ?= $(ART_TEST_FULL)
+ART_TEST_JIT := $(ART_TEST_FULL)
 
 # Do you want optimizing compiler tests run?
-ART_TEST_OPTIMIZING ?= $(ART_TEST_FULL)
+ART_TEST_OPTIMIZING := $(ART_TEST_FULL)
 
 # Do we want to test a PIC-compiled core image?
-ART_TEST_PIC_IMAGE ?= $(ART_TEST_FULL)
+ART_TEST_PIC_IMAGE := $(ART_TEST_FULL)
 
 # Do we want to test PIC-compiled tests ("apps")?
-ART_TEST_PIC_TEST ?= $(ART_TEST_FULL)
+ART_TEST_PIC_TEST := $(ART_TEST_FULL)
 
 # Do you want tracing tests run?
-ART_TEST_TRACE ?= $(ART_TEST_FULL)
+ART_TEST_TRACE := $(ART_TEST_FULL)
 
 # Do you want tracing tests (streaming mode) run?
-ART_TEST_TRACE_STREAM ?= $(ART_TEST_FULL)
+ART_TEST_TRACE_STREAM := $(ART_TEST_FULL)
 
 # Do you want tests with GC verification enabled run?
-ART_TEST_GC_VERIFY ?= $(ART_TEST_FULL)
+ART_TEST_GC_VERIFY := $(ART_TEST_FULL)
 
 # Do you want tests with the GC stress mode enabled run?
-ART_TEST_GC_STRESS ?= $(ART_TEST_FULL)
+ART_TEST_GC_STRESS := $(ART_TEST_FULL)
 
 # Do you want tests with the JNI forcecopy mode enabled run?
-ART_TEST_JNI_FORCECOPY ?= $(ART_TEST_FULL)
+ART_TEST_JNI_FORCECOPY := $(ART_TEST_FULL)
 
 # Do you want run-tests with relocation disabled run?
-ART_TEST_RUN_TEST_NO_RELOCATE ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_NO_RELOCATE := $(ART_TEST_FULL)
 
 # Do you want run-tests with prebuilding?
-ART_TEST_RUN_TEST_PREBUILD ?= true
+ART_TEST_RUN_TEST_PREBUILD := $(ART_TEST_FULL)
 
 # Do you want run-tests with no prebuilding enabled run?
-ART_TEST_RUN_TEST_NO_PREBUILD ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_NO_PREBUILD := $(ART_TEST_FULL)
 
 # Do you want run-tests without a pregenerated core.art?
-ART_TEST_RUN_TEST_NO_IMAGE ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_NO_IMAGE := $(ART_TEST_FULL)
 
 # Do you want run-tests with relocation enabled but patchoat failing?
-ART_TEST_RUN_TEST_RELOCATE_NO_PATCHOAT ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_RELOCATE_NO_PATCHOAT := $(ART_TEST_FULL)
 
 # Do you want run-tests without a dex2oat?
-ART_TEST_RUN_TEST_NO_DEX2OAT ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_NO_DEX2OAT := $(ART_TEST_FULL)
 
 # Do you want run-tests with libartd.so?
-ART_TEST_RUN_TEST_DEBUG ?= true
+ART_TEST_RUN_TEST_DEBUG := $(ART_TEST_FULL)
 
 # Do you want run-tests with libart.so?
-ART_TEST_RUN_TEST_NDEBUG ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_NDEBUG := $(ART_TEST_FULL)
 
 # Do you want run-tests with the host/target's second arch?
-ART_TEST_RUN_TEST_2ND_ARCH ?= true
+ART_TEST_RUN_TEST_2ND_ARCH := $(ART_TEST_FULL)
 
 # Do you want failed tests to have their artifacts cleaned up?
 ART_TEST_RUN_TEST_ALWAYS_CLEAN ?= true
 
 # Do you want run-tests with the --debuggable flag
-ART_TEST_RUN_TEST_DEBUGGABLE ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_DEBUGGABLE := $(ART_TEST_FULL)
 
 # Define the command run on test failure. $(1) is the name of the test. Executed by the shell.
 define ART_TEST_FAILED

Inventor1937/carbon_art

Change-Id: Iee15e74dc14fe4c0697c5fbe1eeb242c626be0a4

build/Android.common_test.mk
@@ -39,7 +39,7 @@ ART_TEST_RUN_TEST_SKIP ?=
 ART_TEST_KNOWN_FAILING :=
 
 # Keep going after encountering a test failure?
-ART_TEST_KEEP_GOING ?= true
+ART_TEST_KEEP_GOING := true
 
 # Do you want all tests, even those that are time consuming?
 ART_TEST_FULL ?= false
@@ -48,71 +48,71 @@ ART_TEST_FULL ?= false
 ART_TEST_QUIET ?= true
 
 # Do you want interpreter tests run?
-ART_TEST_INTERPRETER ?= $(ART_TEST_FULL)
-ART_TEST_INTERPRETER_ACCESS_CHECKS ?= $(ART_TEST_FULL)
+ART_TEST_INTERPRETER := $(ART_TEST_FULL)
+ART_TEST_INTERPRETER_ACCESS_CHECKS := $(ART_TEST_FULL)
 
 # Do you want JIT tests run?
-ART_TEST_JIT ?= $(ART_TEST_FULL)
+ART_TEST_JIT := $(ART_TEST_FULL)
 
 # Do you want optimizing compiler tests run?
-ART_TEST_OPTIMIZING ?= true
+ART_TEST_OPTIMIZING := true
 
 # Do we want to test a PIC-compiled core image?
-ART_TEST_PIC_IMAGE ?= $(ART_TEST_FULL)
+ART_TEST_PIC_IMAGE := $(ART_TEST_FULL)
 
 # Do we want to test PIC-compiled tests ("apps")?
-ART_TEST_PIC_TEST ?= $(ART_TEST_FULL)
+ART_TEST_PIC_TEST := $(ART_TEST_FULL)
 
 # Do you want tracing tests run?
-ART_TEST_TRACE ?= $(ART_TEST_FULL)
+ART_TEST_TRACE := $(ART_TEST_FULL)
 
 # Do you want tracing tests (streaming mode) run?
-ART_TEST_TRACE_STREAM ?= $(ART_TEST_FULL)
+ART_TEST_TRACE_STREAM := $(ART_TEST_FULL)
 
 # Do you want tests with GC verification enabled run?
-ART_TEST_GC_VERIFY ?= $(ART_TEST_FULL)
+ART_TEST_GC_VERIFY := $(ART_TEST_FULL)
 
 # Do you want tests with the GC stress mode enabled run?
-ART_TEST_GC_STRESS ?= $(ART_TEST_FULL)
+ART_TEST_GC_STRESS := $(ART_TEST_FULL)
 
 # Do you want tests with the JNI forcecopy mode enabled run?
-ART_TEST_JNI_FORCECOPY ?= $(ART_TEST_FULL)
+ART_TEST_JNI_FORCECOPY := $(ART_TEST_FULL)
 
 # Do you want run-tests with relocation disabled run?
-ART_TEST_RUN_TEST_NO_RELOCATE ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_NO_RELOCATE := $(ART_TEST_FULL)
 
 # Do you want run-tests with prebuilding?
-ART_TEST_RUN_TEST_PREBUILD ?= true
+ART_TEST_RUN_TEST_PREBUILD := $(ART_TEST_FULL)
 
 # Do you want run-tests with no prebuilding enabled run?
-ART_TEST_RUN_TEST_NO_PREBUILD ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_NO_PREBUILD := $(ART_TEST_FULL)
 
 # Do you want run-tests without a pregenerated core.art?
-ART_TEST_RUN_TEST_NO_IMAGE ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_NO_IMAGE := $(ART_TEST_FULL)
 
 # Do you want run-tests with relocation enabled but patchoat failing?
-ART_TEST_RUN_TEST_RELOCATE_NO_PATCHOAT ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_RELOCATE_NO_PATCHOAT := $(ART_TEST_FULL)
 
 # Do you want run-tests without a dex2oat?
-ART_TEST_RUN_TEST_NO_DEX2OAT ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_NO_DEX2OAT := $(ART_TEST_FULL)
 
 # Do you want run-tests with libartd.so?
-ART_TEST_RUN_TEST_DEBUG ?= true
+ART_TEST_RUN_TEST_DEBUG := $(ART_TEST_FULL)
 
 # Do you want run-tests with libart.so?
-ART_TEST_RUN_TEST_NDEBUG ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_NDEBUG := $(ART_TEST_FULL)
 
 # Do you want run-tests with the host/target's second arch?
-ART_TEST_RUN_TEST_2ND_ARCH ?= true
+ART_TEST_RUN_TEST_2ND_ARCH := $(ART_TEST_FULL)
 
 # Do you want failed tests to have their artifacts cleaned up?
 ART_TEST_RUN_TEST_ALWAYS_CLEAN ?= true
 
 # Do you want run-tests with the --debuggable flag
-ART_TEST_RUN_TEST_DEBUGGABLE ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_DEBUGGABLE := $(ART_TEST_FULL)
 
 # Do you want to test multi-part boot-image functionality?
-ART_TEST_RUN_TEST_MULTI_IMAGE ?= $(ART_TEST_FULL)
+ART_TEST_RUN_TEST_MULTI_IMAGE := $(ART_TEST_FULL)
 
 # Define the command run on test failure. $(1) is the name of the test. Executed by the shell.
 define ART_TEST_FAILED